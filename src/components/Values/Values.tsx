8cdff1967c7d6cb59cce541c6c4624c189ebb558
deejacodes <74888117+deejaneen@users.noreply.github.com>
2024-09-05 18:04:35 +0800

    Add files via upload

--- src/components/Values/Values.tsx
+++ src/components/Values/Values.tsx
@@ -13,7 +13,13 @@ const Values = () => {
         <div className="values__text-box--text-left">
           <p className="value">
             At the core of my coding and design principles is a commitment to
-            accessibility for all. Using semantic HTML for differently abled people so that we all may access information easier and more directly. My design prioritizes readability and comprehensibility, but imbued with intention and a touch of je ne sais quoi. Recently, I've been putting focus on creating websites that degrade beautifully, emphasizing importance on delivering information first before all.
+            accessibility for all. Using semantic HTML for differently abled
+            people so that we all may access information easier and more
+            directly. My design prioritizes readability and comprehensibility,
+            but imbued with intention and a touch of je ne sais quoi. Recently,
+            I've been putting focus on creating websites that degrade
+            beautifully, emphasizing importance on delivering information first
+            before all.
           </p>
         </div>
         <div className="values__text-box--text-right">
@@ -21,8 +27,8 @@ const Values = () => {
             I like thinking creatively, working fast, yet thoughtfully, on how
             my code can have longevity and scalability -- for others who may
             seek to build upon my work in the future. I always strive to
-            improve, grow, and learn alongside others so that from process to pixels we may all create
-            better work and solutions together.
+            improve, grow, and learn alongside others so that from process to
+            pixels we may all create better work and solutions together.
           </p>
         </div>
       </div>
